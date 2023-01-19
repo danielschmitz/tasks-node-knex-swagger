07f4a8fc67c5aa483c2334f8c9689fedf99363cb
Daniel Schmitz <danieljfa@gmail.com>
2023-01-19 08:04:21 -0300

    ✨ create users table

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
---
+++ src/migrations/20230119105618_create_users.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments('id').primary()
+        table.string('name', 80).notNullable()
+        table.string('email', 80).notNullable()
+        table.string('password', 80).notNullable()
+    })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('users')
+}
