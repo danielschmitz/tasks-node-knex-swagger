977049e4f69ab346fb4ef4e856652ebd7aa1e221
Daniel Schmitz <danieljfa@gmail.com>
2023-01-19 08:06:37 -0300

    🔧 env config

--- knexfile.js
+++ knexfile.js
@@ -20,35 +20,22 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: "knex_migrations",
+      directory: "./src/migrations",
+    },
+    seeds: {
+      directory: "./src/seeds",
     }
   }
 
