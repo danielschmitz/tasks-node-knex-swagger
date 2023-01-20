8dac9eeac6eed4ea6bbfbda24649b89d132d36b8
Daniel Schmitz <danieljfa@gmail.com>
2023-01-20 09:43:46 -0300

    tasks

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -14,7 +14,7 @@ const taskSchema = Joi.object({
         .min(3)
         .max(1000),
     user_id: Joi.number(),
-    category_id: Joi.number(),
+    Task_id: Joi.number(),
 })
 
 router.get('/tasks', utils.checkLogin, async function (req, res, next) {
@@ -55,7 +55,7 @@ router.get('/tasks/:id', utils.checkLogin, async function (req, res, next) {
     const user_id = req.auth.id
     const task = await db('tasks').where({ id: req.params.id, user_id })
     if (!task) {
-        return res.status(404).json({ message: 'Category not found' })
+        return res.status(404).json({ message: 'Task not found' })
     }
     return res.json(task)
 })
@@ -66,20 +66,23 @@ router.post('/tasks', utils.checkLogin, async function (req, res, next) {
     #swagger.summary = '🔒️ Create a task by logged user'
     #swagger.responses[401] = { description: 'Unauthorized' }
     #swagger.responses[500] = { description: 'Authorization header is required' }
-    #swagger.responses[409] = { description: 'Category already exists' }
+    #swagger.responses[409] = { description: 'Task already exists' }
     #swagger.responses[403] = { description: 'Invalid Input' }
-    #swagger.responses[201] = { description: "Category created" }
+    #swagger.responses[201] = { description: "Task created" }
     #swagger.parameters['task'] = {
         in: 'body',
-        description: 'Category Data',
+        description: 'Task Data',
         required: true,
         schema: { 
-            "name": "Category name"            
+            "name": "Task name",
+            "description": "Task description",
+            "category_id": "Category id",
+            "done": "Task is done"
         }
     } 
     */
     const user_id = req.auth.id
-    const name = req.body.name
+    const { name, description, category_id, done } = req.body
 
     const validateSchema = taskSchema.validate({name})
     if (validateSchema.error) {
@@ -90,13 +93,20 @@ router.post('/tasks', utils.checkLogin, async function (req, res, next) {
     // search task by name
     const existing_task = await db('tasks').where({ name, user_id })
     if (existing_task.length > 0) {
-        return res.status(409).json({ message: 'Category already exists' })
+        return res.status(409).json({ message: 'Task already exists' })
     }
 
+    
     // create new task
     const task = await db('tasks')
-        .insert({ name, user_id })
-        .returning(['id','name','user_id'])
+        .insert({ 
+                name, 
+                user_id,
+                description,
+                category_id,
+                done           
+            })
+        .returning(['id','name','description','user_id','done'])
 
     return res.json(task[0])
 })
@@ -106,20 +116,23 @@ router.put('/tasks/:id', utils.checkLogin, async function (req, res, next) {
     #swagger.tags = ['Tasks']
     #swagger.summary = '🔒️ Update a task by logged user'
     #swagger.responses[401] = { description: 'Unauthorized' }
-    #swagger.responses[404] = { description: 'Category not found' }
+    #swagger.responses[404] = { description: 'Task not found' }
     #swagger.responses[500] = { description: 'Authorization header is required' }
-    #swagger.responses[200] = { description: "Category updated" }
+    #swagger.responses[200] = { description: "Task updated" }
     #swagger.parameters['task'] = {
         in: 'body',
-        description: 'Category Data',
+        description: 'Task Data',
         required: true,
         schema: { 
-            "name": "Category name"            
+            "name": "Task name",
+            "description": "Task description",
+            "category_id": "Category id",
+            "done": "Task is done"
         }
     } 
     */
     const user_id = req.auth.id
-    const name = req.body.name
+    const { name, description, category_id, done } = req.body
     const id = req.params.id
 
     const validateSchema = taskSchema.validate({name})
@@ -129,14 +142,14 @@ router.put('/tasks/:id', utils.checkLogin, async function (req, res, next) {
 
     const task_exists = await db('tasks').where({ id, user_id })
     if (!task_exists) {
-        return res.status(404).json({ message: 'Category not found' })
+        return res.status(404).json({ message: 'Task not found' })
     }
 
     // update task
     const task = await db('tasks')
         .where({ id, user_id })
-        .update({ name })
-        .returning(['id','name','user_id'])
+        .update({ name, description, category_id, done })
+        .returning(['id','name','description','category_id','user_id'])
 
     return res.json(task[0])
 })
@@ -146,18 +159,18 @@ router.delete('/tasks/:id', utils.checkLogin, async function (req, res, next) {
     #swagger.tags = ['Tasks']
     #swagger.summary = '🔒️ Delete a task by logged user'
     #swagger.responses[401] = { description: 'Unauthorized' }
-    #swagger.responses[404] = { description: 'Category not found' }
+    #swagger.responses[404] = { description: 'Task not found' }
     #swagger.responses[500] = { description: 'Authorization header is required' }
-    #swagger.responses[204] = { description: "Category deleted" }
+    #swagger.responses[204] = { description: "Task deleted" }
     */
     const user_id = req.auth.id
     const id = req.params.id
     const task = await db('tasks').where({ id, user_id })
     if (task.length === 0) {
-        return res.status(404).json({ message: 'Category not found' })
+        return res.status(404).json({ message: 'Task not found' })
     }
     await db('tasks').where({ id, user_id }).delete()
-    return res.json({ message: 'Category deleted' })
+    return res.json({ message: 'Task deleted' })
 })
 
 module.exports = router
